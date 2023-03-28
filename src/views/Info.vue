1209382d342f530371c3593a4605095e7b8f781f
Oat <oatt16@gmail.com>
2023-03-29 00:13:18 +0700

    Info page HTML CSS changes

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,24 +1,36 @@
 <template>
-  <div class="info">
-    <div>
-      <div >
-        <h1>{{ movieInfo.title }}</h1>
-        <h2>{{ movieInfo.original_title }}</h2>
-        <h3>{{ movieInfo.release_date }}</h3>
-        <p>{{ movieInfo.overview }}</p>
-        <input type="text" v-model='review'>
-        <button @click="addReview">Add</button>
-          <div v-for="(review, key) in reviews" :key ='key' >
-            <div>
-              {{review.review}}
+  <div class="movie-review">
+    <div class="movie-poster">
+      <img :src="getPoster()" style="max-width: 400px;"/>
+      </div>
+      <div class="movie-info">
+        <h1 class="movie-title">{{ movieInfo.title }}</h1>
+        <div class="movie-rating">Rating: {{ movieInfo.rating }}/100</div>
+        <p class="movie-description" style="font-size: 20px">{{ movieInfo.overview }}</p>
+        <hr />
+        <h2 class="review-title">Comments:</h2>
+        <div class="review-form">
+          <form @submit.prevent="">
+            <textarea v-model="commentText" placeholder="Add your review"></textarea>
+            <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div class="review-list" > 
+        <ul style="margin: 0; padding: 0;">
+          <li style="list-style:none">
+          <!-- <li v-for="comment in comments" :key="comment.id"> -->
+            <div class="review-section">
+              <div class="reviewer-name">User 1</div>
+              <div class="review-content">so bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lolso bad lol</div>
             </div>
-          </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
-  
+
 <script>
 import {collection, onSnapshot, doc, getFirestore, addDoc, setDoc, updateDoc, deleteDoc} from 'firebase/firestore'
 //import { assert } from '@vue/compiler-core'
@@ -29,6 +41,7 @@ import axios from 'axios'
       return {
         movieInfo: {},
         movieId: this.$route.params.movieid,
+        moviePoster: '',
         reviews: {},
         review: ''
       }
@@ -67,31 +80,67 @@ import axios from 'axios'
         const dataObj = {review: this.review};
         await setDoc(subDocRef, dataObj);
       },
+    getPoster (){
+    if(this.movieInfo.poster_path != null){
+      return 'https://image.tmdb.org/t/p/original' + this.movieInfo.poster_path;
+    }
+    else return 'https://via.placeholder.com/500x750?text=Poster+Not+Available'
+      }   
     }
   }
-  </script>
+</script>
   
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  /* display: inline-block; */
-  margin: 0 10px;
-}
-a {
-  color: #ffffff;
-}
-p {
-  font-size: 16px;
-  margin-top: 10px;
-}
+  .movie-review {
+    display: flex;
+    flex-direction: row;
+    margin: 20px;
+  }
 
+  .movie-container {
+    display: flex;
+    margin-bottom: 20px;
+  }
+
+  .movie-poster{
+    margin-right: 20px;
+  }
+
+  .movie-rating {
+    font-size: 25px;
+    margin-bottom: 10px;
+  }
+
+  .review-form {
+    margin-bottom: 20px;
+  }
+
+  .review-form textarea {
+    width: 100%;
+    height: 100px;
+    font-size: 20px;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+
+  .review-form button {
+    font-size: 15px;
+    border-width: 5px;
+    border-color: grey;
+  }
+  
+  .review-section{
+    margin-bottom: 30px;
+  }
+
+  .reviewer-name{
+    font-size: 25px;
+  }
+
+  .review-content{
+    font-size: 15px;
+  }
 
 
 </style>
